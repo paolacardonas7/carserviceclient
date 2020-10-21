aed9979791eec2c68a6aa4677e318fc430f1ae1c
Paola Andrea Cardona Salcedo <pao_cardona97@hotmail.com>
2020-10-20 22:21:49 -0500

    server.js fixed

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/carserviceclient'));
+app.use(express.static(__dirname+'/dist/carserviceclient-ingweb'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/carserviceclient/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/carserviceclient-ingweb/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
